7735b56e9e294f9ab73de1971966cb0e01b31bb6
zohraanb <83484386+zohraanb@users.noreply.github.com>
2021-05-01 19:48:36 -0400

    asdsa

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from decouple import config
 
 
 class DiscordBot(discord.Client):
+
+
+    async def on_go():
+        print('Bot is ready')
     async def on_ready(self):
         print("Logged in as {0.user}".format(client))
 
@@ -11,9 +15,16 @@ class DiscordBot(discord.Client):
         if message.author == client.user:
             return
 
-        if message.content.startswith(""):
-            await message.channel.send("")
+        if message.content.startswith("hello"):
+            await message.channel.send("bye")
 
+
+    async def new_member(self, member):
+        await message.channel.send(f'(member) has joined the server.')
+
+
+    async def member_removed(self, member):
+        await message.channel.send(f'(member) has left')
 
 client = DiscordBot()
 client.run(config("DISCORD_TOKEN"))
