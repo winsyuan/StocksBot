6960530d8e3998828c0065b5c67122707556e515
Vito Xie <xiev1@mcmaster.ca>
2021-05-01 19:50:44 -0400

    $graph

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import discord
 import os
 import random
+import matplotlib.pyplot as plt
+import mplfinance as mpf
 from decouple import config
 import yfinance as yf
-
-
 class DiscordBot(discord.Client):
 
     async def on_ready(self):
@@ -17,14 +17,14 @@ class DiscordBot(discord.Client):
             return today_data['Close'][0]
         if message.author == client.user:
             return
-        if message.content.startswith("$buy"):
-            userMessage = message.content.split()
+        if message.content.startswith("$buy"): # The bot outputs the amount of shares that can be bought
+            buyMessage = message.content.split()
             try:
-                stock = userMessage[1]
+                stock = buyMessage[1]
             except:
                 await message.channel.send("No stock entered")
             try:
-                money = userMessage[2]
+                money = buyMessage[2]
             except:
                 await message.channel.send("Amount of money not entered")
             try:
@@ -43,10 +43,19 @@ class DiscordBot(discord.Client):
                 await message.channel.send("```You can buy " + str(stockAmount) + " share of " + stock + " with $" + moneyLeftover + " leftover```")
             else:
                 await message.channel.send("```You can buy " + str(stockAmount) + " shares of " + stock + " with $" + moneyLeftover + " leftover```")
-            stock = yf.Ticker("AMZN") 
-            #hist = stock.history(period="max")
-            #print(current_price('AAPL'))
-            await message.channel.send(stock.sustanability)
-            
+        if message.content.startswith("$graph"): # The bot outputs a graph
+            graphMessage = message.content.split() #  The format is $graph StockName startdate enddate
+            try:
+                stockName = graphMessage[1]
+                startDate = graphMessage[2]
+                endDate = graphMessage[3]
+                interval = graphMessage[4]
+            except:
+                await message.channel.send("Please enter the correct format: ```$graph StockName YYYY-MM-DD YYYY-MM-DD 1m/1h/1d/1wk/1mo```")
+            stock = yf.Ticker(stockName) 
+            hist = stock.history(start=startDate, end=endDate,interval = interval,actions=False)
+            mpf.plot(hist,type='candle',volume=True,style='yahoo',mav=(20),title=stockName, tight_layout=True,savefig='plot.png')
+            await message.channel.send(file=discord.File('plot.png'))
+            os.remove('plot.png')            
 client = DiscordBot()
 client.run(config("DISCORD_TOKEN"))
