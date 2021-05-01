60c4af4094985e75ff69bfe5f4cde0ed1ae4b9a4
Vito Xie <xiev1@mcmaster.ca>
2021-05-01 17:04:19 -0400

    number of shares that can be bought

--- main.py
+++ main.py
@@ -11,16 +11,32 @@ class DiscordBot(discord.Client):
         print("Logged in as {0.user}".format(client))
 
     async def on_message(self, message):
-        def current_price(symbol):
+        def current_price(symbol): # function to get current price of stock
             ticket = yf.Ticker(symbol)
             today_data = ticket.history(period='1d')
             return today_data['Close'][0]
         if message.author == client.user:
             return
-        if message.content == ("$ticket"):
-            stock = yf.Ticker("AAPL")
-            hist = stock.history(period="max")
-            print(current_price('AAPL'))
+        if message.content.startswith("$ticket"):
+            userMessage = message.content.split()
+            try:
+                stock = userMessage[1]
+            except:
+                await message.channel.send("No stock entered")
+            try:
+                money = userMessage[2]
+            except:
+                await message.channel.send("Not a valid price")
+            try:
+                stockPrice = current_price(stock)
+            except:
+                await message.channel.send("Not a valid stock")
+            stockAmount = float(money)//float(stockPrice)
+            moneyLeftover = float(money)%float(stockPrice)
+            await message.channel.send("You can buy " + str(stockAmount) + " stocks of " + stock + " with $" + str(moneyLeftover) + " leftover")
+            #stock = yf.Ticker("AAPL")
+            #hist = stock.history(period="max")
+            #print(current_price('AAPL'))
             #await message.channel.send(aapl)
 client = DiscordBot()
 client.run(config("DISCORD_TOKEN"))
