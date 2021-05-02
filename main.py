f3d6a0662c0c763a7b67cbbee6135282518043cd
zohraanb <83484386+zohraanb@users.noreply.github.com>
2021-05-02 04:05:45 -0400

    DMbot final
    this what i got so far idk how to change the format

--- main.py
+++ main.py
@@ -24,29 +24,36 @@ async def on_message(message):
 @client.event
 async def on_member_join(member):
     await member.send('Welcome to the RUhacks stonks server!')
-    await member.send('Are you a new to investing?')
+    await member.send('Are you a new to investing?(yes/no)')
 
-   # if member.content.startswith("yes"):
-    #    await member.send('You can start by making an account an online broker')
+
+#DMS a new client
 @client.event
 async def on_message(message):
+    if message.author == client.user:
+        return
 
     if not message.guild:
-        if message.content.startswith('yes') or message.content.startswith('Yes'):
-            await message.channel.send('')
-
-        elif message.content.startswith('no') or message.content.startswith('No'):
+#Answer yes output
+       if  message.content.startswith('yes') or message.content.startswith('Yes'):
+            await message.channel.send('How you can get started:'
+                                       '\n1. You should open an account with a stocks brokerage('
+                                       'ex. Wealthsimple, Investing through your bank, Questrade, etc'
+                                       '\n2. Open a Tax Free Savings Account(TFSA), this will save you from paying any '
+                                       'taxes on the profits you make.'
+                                       '\n3. Research companies and find stocks you are interested'
+                                       '\n4. When you you find a good entry point use your TFSA and start investing'
+                                       '\n\n Now that you are ready, this server will help you find stocks, the prices and '
+                                       'trends in the market')
             await message.channel.send('Enter "$help" to find commands you can use in this server for analyzing stock')
+#Answer with no
+       elif message.content.startswith('no') or message.content.startswith('No'):
+            await message.channel.send('Enter "$help" to find commands you can use in this server for analyzing stock')
+#Invalid input
+       elif len(message.content) > 1 and not message.content.startswith('$help') and not message.content.startswith('$active') and \
+               not message.content.startswith('$buy') and not message.content.startswith('$graph'):
+            await message.channel.send('Invalid input, please enter a valid input.')
 
-        else (len(message.content) > 0)
-            await message.channel.send('Invalid input')
-
-
-
-@client.event
-async def on_member_remove(member):
-
-    await member.send('user left')
 
 
 client.run(config("DISCORD_TOKEN"))
