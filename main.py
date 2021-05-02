c97eb87ff9b9508b397f28ac9b661801c956344e
zohraanb <83484386+zohraanb@users.noreply.github.com>
2021-05-02 00:31:07 -0400

    DM Messages

--- main.py
+++ main.py
@@ -23,11 +23,29 @@ async def on_message(message):
 
 @client.event
 async def on_member_join(member):
-    await member.send('user joined')
+    await member.send('Welcome to the RUhacks stonks server!')
+    await member.send('Are you a new to investing?')
+
+   # if member.content.startswith("yes"):
+    #    await member.send('You can start by making an account an online broker')
+@client.event
+async def on_message(message):
+
+    if not message.guild:
+        if message.content.startswith('yes') or message.content.startswith('Yes'):
+            await message.channel.send('')
+
+        elif message.content.startswith('no') or message.content.startswith('No'):
+            await message.channel.send('Enter "$help" to find commands you can use in this server for analyzing stock')
+
+        else (len(message.content) > 0)
+            await message.channel.send('Invalid input')
+
 
 
 @client.event
 async def on_member_remove(member):
+
     await member.send('user left')
 
 
