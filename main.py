8e9a916f74b7c26a01b4008294079fe422e36acb
winson <59904327+winsyuan@users.noreply.github.com>
2021-05-01 22:19:31 -0400

    refactor: remove discord subclass

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import yfinance as yf
 from bs4 import BeautifulSoup
 import requests
 
+intents = discord.Intents.default()
+
+intents.members = True
+client = discord.Client(intents=intents)
+
 
 def valid_stock_check(stock):
     if len(stock.info) == 1:
@@ -27,73 +32,68 @@ def scrap_yahoo_trending_stocks():
     ]
 
 
-class DiscordBot(discord.Client):
-    async def on_ready(self):
-        print("Logged in as {0.user}".format(client))
+@client.event
+async def on_ready():
+    print("Logged in as {0.user}".format(client))
 
-    async def on_message(self, message):
-        if message.author == client.user:
-            return
 
-        """
-            Command that gets more information on a stock
-            $ticket [req: symbol]
-            ex: $ticket AAPL
-        """
-        if message.content.startswith("$ticket"):
-            input_message = message.content.split(" ")
-            length = len(input_message)
-            output_message = ""
-            if length == 1:
-                output_message = (
-                    "Missing required symbol parameter try `$ticket (SYMBOL)`"
-                )
-            else:
-                ticket = input_message[1]
-                stock = yf.Ticker(ticket)
-                valid = valid_stock_check(stock)
-                if valid:
-                    if length == 2:
-                        output_message = (
-                            "**Information about " + ticket.upper() + "** \n"
-                        )
-                        data = stock.history(period="1d")
-                        output_message += (
-                            "Company Name: "
-                            + stock.info["longName"]
-                            + "\n"
-                            + "Opening Stock Price: "
-                            + str(round(data["Open"][0], 3))
-                            + "\n"
-                            + "Closing Stock Price: "
-                            + str(round(data["Close"][0], 3))
-                            + "\n"
-                            + "Stock Volume: "
-                            + str(round(data["Volume"][0], 3))
-                        )
-                else:
-                    output_message = (
-                        "Stock not found, try again with a different symbol"
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    """
+        Command that gets more information on a stock
+        $ticket [req: symbol]
+        ex: $ticket AAPL
+    """
+    if message.content.startswith("$ticket"):
+        input_message = message.content.split(" ")
+        length = len(input_message)
+        output_message = ""
+        if length == 1:
+            output_message = "Missing required symbol parameter try `$ticket (SYMBOL)`"
+        else:
+            ticket = input_message[1]
+            stock = yf.Ticker(ticket)
+            valid = valid_stock_check(stock)
+            if valid:
+                if length == 2:
+                    output_message = "**Information about " + ticket.upper() + "** \n"
+                    data = stock.history(period="1d")
+                    output_message += (
+                        "Company Name: "
+                        + stock.info["longName"]
+                        + "\n"
+                        + "Opening Stock Price: "
+                        + str(round(data["Open"][0], 3))
+                        + "\n"
+                        + "Closing Stock Price: "
+                        + str(round(data["Close"][0], 3))
+                        + "\n"
+                        + "Stock Volume: "
+                        + str(round(data["Volume"][0], 3))
                     )
-            await message.channel.send(output_message)
+            else:
+                output_message = "Stock not found, try again with a different symbol"
+        await message.channel.send(output_message)
 
-        if message.content == "$active":
-            output_message = "**Most Active Stocks**```\n"
-            data = scrap_yahoo_trending_stocks()
-            for i in range(len(data)):
-                output_message += (
-                    data[i]["name"]
-                    + " "
-                    + data[i]["ticket"]
-                    + " $"
-                    + data[i]["price"]
-                    + " "
-                    + data[i]["change_percentage"]
-                    + "\n"
-                )
-            output_message += "```"
-            await message.channel.send(output_message)
+    if message.content == "$active":
+        output_message = "**Most Active Stocks**```\n"
+        data = scrap_yahoo_trending_stocks()
+        for i in range(len(data)):
+            output_message += (
+                data[i]["name"]
+                + " "
+                + data[i]["ticket"]
+                + " $"
+                + data[i]["price"]
+                + " "
+                + data[i]["change_percentage"]
+                + "\n"
+            )
+        output_message += "```"
+        await message.channel.send(output_message)
 
 
-client = DiscordBot()
 client.run(config("DISCORD_TOKEN"))
