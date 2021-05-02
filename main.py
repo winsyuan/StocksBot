cae03075ef1291166590f3c60d40d8c89aa1d70e
winson <59904327+winsyuan@users.noreply.github.com>
2021-05-02 10:12:45 -0400

    chore: ran black .

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+    print("Logged in as {0.user}".format(client))
 
 
 @client.event
@@ -23,37 +23,47 @@ async def on_message(message):
 
 @client.event
 async def on_member_join(member):
-    await member.send('Welcome to the RUhacks stonks server!')
-    await member.send('Are you a new to investing?(yes/no)')
+    await member.send("Welcome to the RUhacks stonks server!")
+    await member.send("Are you a new to investing?(yes/no)")
 
 
-#DMS a new client
+# DMS a new client
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
     if not message.guild:
-#Answer yes output
-       if  message.content.startswith('yes') or message.content.startswith('Yes'):
-            await message.channel.send('How you can get started:'
-                                       '\n1. You should open an account with a stocks brokerage('
-                                       'ex. Wealthsimple, Investing through your bank, Questrade, etc'
-                                       '\n2. Open a Tax Free Savings Account(TFSA), this will save you from paying any '
-                                       'taxes on the profits you make.'
-                                       '\n3. Research companies and find stocks you are interested'
-                                       '\n4. When you you find a good entry point use your TFSA and start investing'
-                                       '\n\n Now that you are ready, this server will help you find stocks, the prices and '
-                                       'trends in the market')
-            await message.channel.send('Enter "$help" to find commands you can use in this server for analyzing stock')
-#Answer with no
-       elif message.content.startswith('no') or message.content.startswith('No'):
-            await message.channel.send('Enter "$help" to find commands you can use in this server for analyzing stock')
-#Invalid input
-       elif len(message.content) > 1 and not message.content.startswith('$help') and not message.content.startswith('$active') and \
-               not message.content.startswith('$buy') and not message.content.startswith('$graph'):
-            await message.channel.send('Invalid input, please enter a valid input.')
-
+        # Answer yes output
+        if message.content.startswith("yes") or message.content.startswith("Yes"):
+            await message.channel.send(
+                "How you can get started:"
+                "\n1. You should open an account with a stocks brokerage("
+                "ex. Wealthsimple, Investing through your bank, Questrade, etc"
+                "\n2. Open a Tax Free Savings Account(TFSA), this will save you from paying any "
+                "taxes on the profits you make."
+                "\n3. Research companies and find stocks you are interested"
+                "\n4. When you you find a good entry point use your TFSA and start investing"
+                "\n\n Now that you are ready, this server will help you find stocks, the prices and "
+                "trends in the market"
+            )
+            await message.channel.send(
+                'Enter "$help" to find commands you can use in this server for analyzing stock'
+            )
+        # Answer with no
+        elif message.content.startswith("no") or message.content.startswith("No"):
+            await message.channel.send(
+                'Enter "$help" to find commands you can use in this server for analyzing stock'
+            )
+        # Invalid input
+        elif (
+            len(message.content) > 1
+            and not message.content.startswith("$help")
+            and not message.content.startswith("$active")
+            and not message.content.startswith("$buy")
+            and not message.content.startswith("$graph")
+        ):
+            await message.channel.send("Invalid input, please enter a valid input.")
 
 
 client.run(config("DISCORD_TOKEN"))
